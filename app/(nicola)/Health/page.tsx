bda43824beab25b6676223df6a527fae2bfee42a
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-06-24 20:23:58 +0600

    nomba bro

--- app/(nicola)/Health/page.tsx
+++ app/(nicola)/Health/page.tsx
@@ -1,28 +1,9 @@
 import React from 'react'
-import Image from 'next/image'
-import crystal from "../img/crystal.jpg"
+
 export default function page() {
   return (
- <div className="health  grid grid-cols-1 md:grid-cols-2 p-5 gap-4
-  ">
-   <div className="left   text-6xl font-bold capitalize
-   my-auto">
-         <p className=''>find the perfect</p>
-         <p>health care</p>
-         <p className='text-sm'>Health is a state of complete physical, mental and social well-being and not merely the absence of disease or infirmity</p>
-     </div>
-     <div className="right">
-      <div className="drug">
-      <div className="img w-5/6 h-full px-5  rounded mx-auto">
-        <Image alt='crystal' className='crystal rounded' src={crystal}></Image>
-      </div>
-      <div className="text p-5 mx-auto  w-5/6">
-        <p className='text-4xl font-black capitalize'>crystal meth</p>
-        <p>Crystal meth, or methamphetamine, is a stimulant drug that carries a high risk for physical dependence.</p>
-      </div>
-      </div>
-     </div>
- </div>
-
+    <div>
+        health
+    </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
 import React from 'react'
-import Health  from "./(nicola)/Health/page"
+import Navbar from './(nicola)/Navbar'
+import Health from "./(nicola)/Health/page"
 import Care from "./(nicola)/Care/page"
 import Support from "./(nicola)/Support/page"
-import Nav from "./(nicola)/Nav/page"
-import Contact from "./(nicola)/Contact/page"
-
 export default function page() {
   return (
     <div>
-
-      <Nav/>
+      <Navbar/>
       <Health/>
       <Care/>
       <Support/>
-      <Contact/>
-      
     </div>
   )
 }
+
 
