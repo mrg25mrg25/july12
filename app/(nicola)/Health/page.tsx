37ab76608e28c0fcd4d68e2e6d82b3f272acbda7
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-05 11:57:13 +0600

    p9

--- app/(nicola)/Health/page.tsx
+++ app/(nicola)/Health/page.tsx
@@ -1,12 +1,9 @@
-
 import React from 'react'
-
 
 export default function page() {
   return (
-    <div className='section_health'>
-      <p>ki khobor valo ni</p>
-      <p>khela hobe</p>
+    <div>
+   <a href="https://www.prothomalo.com/" target='l'>purai pinik</a>
     </div>
   )
 }
