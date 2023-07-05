92360cab078ddab73c4a617b12ed0b1f44b858f3
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-05 11:39:55 +0600

    p0

--- app/(nicola)/Health/page.tsx
+++ app/(nicola)/Health/page.tsx
@@ -5,25 +5,8 @@ import img_health from "../img/dr.jpg"
 
 export default function page() {
   return (
-    <div className='health'>
-      <div className="main sm:p-12 py-12 flex flex-row flex-wrap justify-between items-center gap-2 ">
-        <div className="left capitalize mx-auto text-center sm:text-left">
-          <div className='text-5xl drop-shadow-md '>
-            <p className='drop-shadow-md '>your future depends</p>
-            <p> on your health</p>
-            <p>bitch</p>
-           </div>
-          <p className='mt-2'>isn't it tine to work on your own body?</p>
-          <a href="https://www.who.int/"target='blank' className=''><p className='py-2 mx-auto sm:ml-0
-          w-24 text-center rounded mt-4 px-3 bg-cyan-400' >let's start</p></a>
-        </div>
-        <div className="right border-2 mx-auto ">
-          <div className="img_health border-4 w-48">
-          <Image alt='ok' src={img_health} className='w-full h-full'></Image>
-          </div>
-          <p className='uppercase text-center tracking-wider'>dr. nicola goodman</p>
-        </div>
-      </div>
+    <div className='section_health'>
+      <p>ki khobor valo ni</p>
     </div>
   )
 }
