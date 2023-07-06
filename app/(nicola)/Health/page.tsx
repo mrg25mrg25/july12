aefe800887d5055fb2a9e20c151804e6ae88bfd0
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-07 03:05:52 +0600

    a

--- app/(nicola)/Health/page.tsx
+++ app/(nicola)/Health/page.tsx
@@ -3,11 +3,7 @@ import React from 'react'
 export default function page() {
   return (
    <div className="main py-20">
-     <div className="text capitalize text-center font-bold tracking-wider">
-      <p className='text-5xl'>your life depends</p> 
-      <p className='text-4xl'>on your health</p>
-      <p>isn't it time to focous on your own health?</p>
-    </div>
+     <p>aibar ki korba</p>
   </div>
   )
 }
