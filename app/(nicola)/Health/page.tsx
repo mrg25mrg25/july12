ca3662b8da323ff2e5452f5e3a54270b56e56224
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-07 03:22:29 +0600

    no p

--- app/(nicola)/Health/page.tsx
+++ app/(nicola)/Health/page.tsx
@@ -3,14 +3,19 @@ import React from 'react'
 export default function page() {
   return (
   <div className="health">
-    <div className="main">
-      <div className="left">
-      <p className='text-5xl'>your life depends</p> 
+    
+    <div className="main py-20">
+      <div className="text capitalize text-center font-bold tracking-wider">
+        <div className='text-5xl'>your life depends</div> 
+        <div className='text-4xl'>on your health</div>
+        <div>isn't it time to focous on your own health?</div>
       </div>
-      <div className="right">
-        right
+      <div className="description">
+        <p className='w-3/5 mx-auto text-center'>What do you mean by human health?
+          Human health, defined as the complete state of physical, social, and mental well-being and not merely the absence of illness, disease, or infirmity, is as vital a resource as water, food, or energy.
+        </p>
       </div>
-    </div>
+     </div>
   </div>
   )
 }
