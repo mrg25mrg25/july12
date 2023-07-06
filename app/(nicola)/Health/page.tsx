6d4af24c03e1cac533cb4e691f7bbbd662932a90
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-07 03:23:45 +0600

    ok

--- app/(nicola)/Health/page.tsx
+++ app/(nicola)/Health/page.tsx
@@ -5,13 +5,9 @@ export default function page() {
   <div className="health">
     
     <div className="main py-20">
-      <div className="text capitalize text-center font-bold tracking-wider">
-        <div className='text-5xl'>your life depends</div> 
-        <div className='text-4xl'>on your health</div>
-        <div>isn't it time to focous on your own health?</div>
-      </div>
+      
       <div className="description">
-        <p className='w-3/5 mx-auto text-center'>What do you mean by human health?
+         <p className='w-3/5 mx-auto text-center'>What do you mean by human health?
           Human health, defined as the complete state of physical, social, and mental well-being and not merely the absence of illness, disease, or infirmity, is as vital a resource as water, food, or energy.
         </p>
       </div>
