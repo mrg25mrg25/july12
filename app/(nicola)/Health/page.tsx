947adf868d10f12bdb6a64d8911102c9f67eec60
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-07 03:19:11 +0600

    left

--- app/(nicola)/Health/page.tsx
+++ app/(nicola)/Health/page.tsx
@@ -5,7 +5,9 @@ export default function page() {
   <div className="health">
     <div className="main">
       <div className="left">
-        left
+      <p className='text-5xl'>your life depends</p> 
+        <p className='text-4xl'>on your health</p>
+        <p>isn't it time to focous on your own health?</p>
       </div>
       <div className="right">
         right
