b86ed353821f42c05d2d1835c5758ef92611270c
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-07 03:25:58 +0600

    asshole

--- app/(nicola)/Health/page.tsx
+++ app/(nicola)/Health/page.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 export default function page() {
   return (
   <div className="health">
-    
     <div className="main py-20">
-      
+      <div className="text">
+        <p>fuck you</p>
+        <p>dirty asshole</p>
+      </div>
       <div className="description">
          <p className='w-3/5 mx-auto text-center'>What do you mean by human health?
           Human health, defined as the complete state of physical, social, and mental well-being and not merely the absence of illness, disease, or infirmity, is as vital a resource as water, food, or energy.
